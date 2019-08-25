2cfe4a706507cb99a35fbda139a527de6d524b9f
Allison French <alliefrench22@gmail.com>
2019-08-25 10:52:44 -0400

    adds navbar component and styling

--- client/index.js
+++ client/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store/index';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
