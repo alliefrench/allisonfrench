cd09bce6250dc91ed85ed8f357427097540e8d73
Allison French <alliefrench22@gmail.com>
2019-08-29 14:52:53 -0400

    working on firebase connectivity

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Post } from './index';
+import firebase from 'firebase';
 
 const image = 'code.jpeg';
 const headline = '10 Things I Love About Coding';
@@ -8,15 +9,33 @@ const date = 'August 28, 2019';
 const text =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod lacinia at quis risus sed vulputate odio ut. Augue eget arcu dictum varius duis.';
 
-const Posts = () => {
-  return (
-    <div className="posts_page">
-      <div className="posts_page_column">
-        <div className="posts_callout">Musings about tech and life in NYC</div>
-        <Post image={image} title={headline} date={date} body={text} />
+class Posts extends React.Component {
+  constructor() {
+    super();
+    this.state = { posts: [] };
+    this.getPosts = this.getPosts.bind(this);
+  }
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  async getPosts() {
+    const posts = await firebase.firestore.Query.get(posts);
+    this.setState({ ...this.state, posts });
+  }
+
+  render() {
+    return (
+      <div className="posts_page">
+        <div className="posts_page_column">
+          <div className="posts_callout">
+            Musings about tech and life in NYC
+          </div>
+          <Post image={image} title={headline} date={date} body={text} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Posts;
---
+++ init-firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'allisonfrench-17757.firebaseapp.com',
+  databaseURL: 'https://allisonfrench-17757.firebaseio.com',
+  projectId: 'allisonfrench-17757',
+  storageBucket: '',
+  messagingSenderId: '520869181406',
+  appId: '1:520869181406:web:14afcdfb52461a54',
+};
