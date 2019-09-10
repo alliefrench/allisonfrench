6ace27de9b336bb3486ff94d673dfed6ba766630
Allison French <alliefrench22@gmail.com>
2019-09-10 16:07:49 -0400

    removes unused code

--- init-firebase.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: 'allisonfrench-17757.firebaseapp.com',
-  databaseURL: 'https://allisonfrench-17757.firebaseio.com',
-  projectId: 'allisonfrench-17757',
-  storageBucket: '',
-  messagingSenderId: '520869181406',
-  appId: '1:520869181406:web:14afcdfb52461a54',
-};
