c1ba81ecd89209ffcf84111b64993487d5990038
Allison French <alliefrench22@gmail.com>
2019-09-10 14:36:09 -0400

    Posts API retrieving data from Firebase

--- server/api/posts.js
+++ server/api/posts.js
@@ -1,19 +1,36 @@
 const router = require('express').Router();
 const db = require('../db');
 
-router.get('/', async function(req, res, next) {
-  let postRef = await db.collection('posts').doc('1');
-  let getDoc = postRef
+router.get('/', function(req, res, next) {
+  let postRef = db.collection('posts');
+  let response = [];
+  let allPosts = postRef
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        response.push(doc.data());
+      });
+      res.send(response);
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+});
+
+router.get('/:postId', function(req, res, next) {
+  let postRef = db.collection('posts').doc(req.params.postId);
+  let post = postRef
     .get()
     .then(doc => {
       if (!doc.exists) {
-        console.log('No such document!');
+        res.sendStatus(404);
       } else {
         res.send(doc.data());
       }
     })
     .catch(err => {
-      console.log('Error getting document', err);
+      console.log('Error getting documents', err);
     });
 });
+
 module.exports = router;
